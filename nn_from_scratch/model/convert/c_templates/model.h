f25bfd3bbb499d64846bd881705aed1a6df37e08
simon <yconsj@gmail.com>
2024-03-25 13:54:42 +0100

    requested changes

--- nn_from_scratch/model/convert/c_templates/model.h
+++ nn_from_scratch/model/convert/c_templates/model.h
@@ -1,19 +1,10 @@
-#include <stdint.h>
+#include <cstdint>
+#include "model.h"
 
-#define INPUT_SIZE 1
-#define N_LAYERS 3
+{layer_weights}
+{layer_biases}
 
-#define LAYER_0_SIZE 4
-#define LAYER_1_SIZE 2
-#define LAYER_2_SIZE 1
-
-
-enum ActivationType {
-    LINEAR,
-    RELU
-};
-
-extern int layers_size[N_LAYERS];
-extern float* layers_weights[N_LAYERS];     // shape: (n_layers)(input_size * output_size)
-extern float* layers_biases[N_LAYERS];      // shape: (n_layers)(output_size)
-extern enum ActivationType layers_activation[N_LAYERS];
+int layers_size[N_LAYERS] = {{layers_size}};
+float* layers_weights[N_LAYERS] = {{layers_weights}};
+float* layers_biases[N_LAYERS] = {{layers_biases}};
+enum ActivationType layers_activation[N_LAYERS] = {{layers_activation}};
